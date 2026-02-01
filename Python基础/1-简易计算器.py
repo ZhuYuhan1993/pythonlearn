ecd1f4b2abf21b38ee29335b55dd9cd5e7c9c31e
ZhuYuhan <806271598@qq.com>
2026-02-01 12:14:13 +0800

    实现了用户指定运算符，以及多次计算的功能，下一步是支持处理多个数字和运算符的功能

--- Python基础/1-简易计算器.py
+++ Python基础/1-简易计算器.py
@@ -6,15 +6,40 @@
 3. 注意：input()得到的是字符串，需要转换
 """
 from logging import exception
+import math
+def calculator():
+    calculate_flag = 1
+    while calculate_flag:
+        a,b = input('请输入两个数字，用空格隔开').split()
+        a = int(a)
+        b = int(b)
+        operator = input('请输入你想使用的运算符+、-、*、/、^、')
+        if operator == '+':
+            print(a+b)
+            calculate_flag = 0
+        elif operator == '-':
+            print(a-b)
+            calculate_flag = 0
+        elif operator == '*':
+            print(a*b)
+            calculate_flag = 0
+        elif operator == '/':
+            if b!=0:
+                print(a/b)
+                calculate_flag = 0
+            else:
+                print('b为0，不能作分母')
+                calculate_flag = 0
+        elif operator == '^':
+            # print(math.pow(a,b))
+            print(a**b)
+            calculate_flag = 0
 
-try:
-    a,b = input('输入两个数字:').split()
-except Exception as e:
-    print(f"报错：{e}")
-print(f'a+b:{int(a)+int(b)}')
-print(f'a-b:{int(a)-int(b)}')
-print(f'a*b:{int(a)*int(b)}')
-if b!=0:
-    print(f'a/b:{int(a)/int(b)}')
-else:
-    print(f'b为0')
+        else:
+            print('没有匹配到指定的运算符')
+        action = input('请指定下一步操作：1.继续运算,否则任意键退出')
+        if action == '1':
+            calculate_flag = 1
+
+if __name__ == '__main__':
+    calculator()
